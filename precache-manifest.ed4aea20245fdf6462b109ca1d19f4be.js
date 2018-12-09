ebf1ccda6b73f41c2850b7c89512cede9754bd08
thedanwoods <dan@danwoods.co.uk>
2018-12-09 23:19:04 +0000

    Updates

---
+++ precache-manifest.ed4aea20245fdf6462b109ca1d19f4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29e960f41c9fd22d2638",
+    "url": "/movie-listings/static/js/runtime~main.29e960f4.js"
+  },
+  {
+    "revision": "0fb20fcf6296c64611a8",
+    "url": "/movie-listings/static/js/main.0fb20fcf.chunk.js"
+  },
+  {
+    "revision": "f2e530b54e8d7c2fe35d",
+    "url": "/movie-listings/static/js/1.f2e530b5.chunk.js"
+  },
+  {
+    "revision": "0fb20fcf6296c64611a8",
+    "url": "/movie-listings/static/css/main.ad63f711.chunk.css"
+  },
+  {
+    "revision": "f2e530b54e8d7c2fe35d",
+    "url": "/movie-listings/static/css/1.58e01894.chunk.css"
+  },
+  {
+    "revision": "b9540421be0c83a82ac42110d1b9d1c0",
+    "url": "/movie-listings/index.html"
+  }
+];
